4a1bd6e87f312412310ba294376eacc3b744af45
川建国同志的奇妙加农炮之旅 <55585958+HYBB-rash@users.noreply.github.com>
2020-08-08 10:01:03 +0800

    打印token模块完成

--- include/utils.h
+++ include/utils.h
@@ -5,6 +5,13 @@
 #ifndef COMPLIER_HY_UTILS_H
 #define COMPLIER_HY_UTILS_H
 
+#include "../include/tokens.h"
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
 
+void outTokenTemplate(const string& s1, const string& s2);
+void printToken(token t);
 
 #endif //COMPLIER_HY_UTILS_H

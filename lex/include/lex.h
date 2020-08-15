7ee062d873d8ea858618b5a9d41d27e4e0a6db07
hyong <hyong@hybing.onmicrosoft.com>
2020-08-15 19:04:54 +0800

    重置lex文件

--- lex/include/lex.h
+++ lex/include/lex.h
@@ -4,9 +4,12 @@
 
 #ifndef COMPILER_HY_LEX_H
 #define COMPILER_HY_LEX_H
+#include <vector>
+#include "../include/tokens.h"
 /*
  * 词法分析器调用的函数，lex函数可以自动读取输入的所有字符串直到EOF为止
  * 暂时来说开发到输出token类型的地步，下一次会和语法分析器结合
  */
-void lex();
+
+vector<token> lex();
 #endif //COMPILER_HY_LEX_H
